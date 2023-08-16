f00f324bea40b613c739d019e3a626233e071288
ChemaCeldran16 <chemaceldranharo@gmail.com>
2023-08-16 16:29:37 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './components/NavBar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="w-screen h-screen dark text-foreground bg-background p-8 flex items-start justify-center">
+        <NavBar />
+      </div>
+      <Routes>
+        <Route path="/" >
+          {/* <Route index element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/information" element={<Information />} />
+            <Route path="/services/training" element={<Information />} />
+            <Route path="/services/inference" element={<Information />} />
+            <Route path="/services/mdk" element={<Information />} />          */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
